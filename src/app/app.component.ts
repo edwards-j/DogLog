c4b9174ca6d2e11f2a7c1cf6037448592715ee7b
edwards-j <jonathan.d.edwards92@gmail.com>
2021-11-06 19:28:44 -0500

    change to sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +10,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DogLog';
+  showNavButton: boolean
+  @ViewChild('sidenav') sidenav: any; 
+
+  constructor(private afAuth: AngularFireAuth, private router: Router, private authService: AuthService) {
+    this.showNavButton = false;
+
+    this.afAuth.onAuthStateChanged(user => {
+      if (user) {
+        this.showNavButton = true
+      } else {
+        this.showNavButton = false
+      }
+    })
+  }
+
+  toggleSideNav() {
+    this.sidenav.toggle()
+  }
+
+  navigateHome(): void {
+    this.router.navigate(['/home']).then(() => {
+      this.toggleSideNav()
+    })
+  }
+
+  logOut() {
+    this.authService.logoutUser()
+  }
 
   goBack() {
     window.history.back()
   }
+  
 }
 
 
